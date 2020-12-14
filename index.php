ea08cb848ecc167bfd8b3eec30d8fb654b55f783
Maurizio <maurizio.faedda1@gmail.com>
2020-12-14 15:15:46 +0100

    Censored a word of the paragraph with GET

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 // scrivo le mie variabili
-    $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+    // badword
+    $my_badword = $_GET['badword'];
+
+    // con cosa voglio rimpiazzare
+    $replace = "***";
+
+    // risultato finale: nuovo paragrafo con censura
+    $new_paragraph = str_replace($my_badword, $replace, $paragraph);
+
+    // lunghezza del paragrafo originale
+    $strlen_paragraph = strlen($paragraph);
+
+    // lunghezza del nuovo paragrafo
+    $strlen_new_paragraph = strlen($new_paragraph);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -9,7 +24,42 @@
         <title></title>
     </head>
     <body>
-        <h1>Title</h1>
-        <p> <?php echo $paragraph ?></p>
+        <small>Per censurare una parola all'interno del paragrafo dai un valore nella querystring a <b>badword</b></small>
+        <h1>Stampa con <em>echo</em></h1>
+        <h2>Censured paragraph</h2>
+
+        <!-- stampo il paragrafo censurato -->
+        <p> <?php echo $new_paragraph ?></p>
+
+        <h3>Original paragraph length vs New paragraph length</h3>
+        <p> Il paragrafo senza censure contiene
+            <b><?php echo $strlen_paragraph ?></b>
+            caratteri.
+            <br>
+            Il paragrafo censurato contiene
+            <b><?php echo $strlen_new_paragraph ?></b>
+            caratteri.
+        </p>
+
+        <hr>
+
+        <h1>Stampa con <em>var_dump</em></h1>
+        <h2>Censured paragraph</h2>
+
+        <!-- stampo il paragrafo censurato -->
+        <p> <?php var_dump($new_paragraph) ?></p>
+
+        <h3>Original paragraph length vs New paragraph length</h3>
+        <p> Il paragrafo senza censure contiene
+            <b><?php var_dump($strlen_paragraph) ?></b>
+            caratteri.
+            <br>
+            Il paragrafo censurato contiene
+            <b><?php var_dump($strlen_new_paragraph) ?></b>
+            caratteri.
+        </p>
+
+
+
     </body>
 </html>
